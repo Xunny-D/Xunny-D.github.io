1ca8a6bdb65c893a13d5cc4940c21f2d4f014965
Danny Xuan Huynh <huynhxuong77@gmail.com>
2023-05-15 18:43:15 -0500

    Revamped design, header and navbar has first complete version

--- README.md
+++ README.md
@@ -9,4 +9,6 @@ Tech Stack: Vanilla JavaScript, HTML, CSS, & Tailwind
 NOTE: Tailwind does not support IE browsers.
 Tailwind CSS v3.0 is designed for and tested on the latest stable versions of Chrome, Firefox, Edge, and Safari.
 
+This site will be done May 19th.
+
 New site built with Nest.js will be live May 25th.
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,36 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage:{
-        'houstonNeon': 'url(/Dependencies/img/computersNeon.JPG)'
+        'vintageNeon': 'url(/Dependencies/img/computersNeon.JPG)',
+        'houstonNeon': 'url(/Dependencies/img/houstonBackgroundCyber.png)',
+        'neonBackground': 'url(/Dependencies/img/neonBackdrop.png)',
+        'neonSpeaker': 'url(/Dependencies/img/neonSpeaker.png)'
       },
       fontFamily: {
-        'Cyberpunk':['Cyberpunk', 'sans-serif']
+        'Cyberpunk':['Cyberpunk', 'sans-serif'],
+        'Revamped':['Revamped', 'sans-serif'],
+        'Hacked':['Hacked', 'sans-serif'],
+        'Alpha':['Alpha', 'sans-serif'],
+        'Bohemian':['Bohemian', 'sans-serif']
       },
+      animation:{
+        'cursor-move': 'spin 3s linear infinite'
+      },
+      height:{
+        '112': '28rem',
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+        '176': '44rem',
+        '192': '48rem',
+        '208': '52rem',
+        '224': '56rem'
+      },
+      fontSize:{
+        '4.2xl': '2.400rem',
+        '4.5xl': '2.600rem',
+        '7.5xl': '5.000rem'
+      }
     },
   },
   plugins: [],
