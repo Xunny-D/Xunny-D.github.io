d5811063dd7b95bbe11c1ca2225822869008440c
Danny Xuan Huynh <huynhxuong77@gmail.com>
2023-05-17 11:41:14 -0500

    Revamped header design second time, scaled large and medium screens, smooth transitions and effects added

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,9 @@ module.exports = {
         'vintageNeon': 'url(/Dependencies/img/computersNeon.JPG)',
         'houstonNeon': 'url(/Dependencies/img/houstonBackgroundCyber.png)',
         'neonBackground': 'url(/Dependencies/img/neonBackdrop.png)',
-        'neonSpeaker': 'url(/Dependencies/img/neonSpeaker.png)'
+        'neonSpeaker': 'url(/Dependencies/img/neonSpeaker.png)',
+        'neonChip': 'url(/Dependencies/img/neonChip.png)',
+        'neonWave': 'url(/Dependencies/img/neonWave.png)'
       },
       fontFamily: {
         'Cyberpunk':['Cyberpunk', 'sans-serif'],
@@ -30,13 +32,26 @@ module.exports = {
         '176': '44rem',
         '192': '48rem',
         '208': '52rem',
-        '224': '56rem'
+        '224': '56rem',
+        '240': '60rem',
+        '256': '64rem',
+        '272': '68rem',
+        '288': '72rem',
+        '304': '76rem',
+        '320': '80rem',
+        '336': '84rem',
+        '400': '100rem',
+        '800': '200rem',
+        '1200': '300rem'
       },
       fontSize:{
         '4.2xl': '2.400rem',
         '4.5xl': '2.600rem',
         '7.5xl': '5.000rem'
-      }
+      },
+      colors:{
+        'neonBlue': '#40A4FF',
+      },
     },
   },
   plugins: [],
